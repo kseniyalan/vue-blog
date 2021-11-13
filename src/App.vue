1e9e52821ccad6957c64a477315d073df99766b8
Ksenia <k.lanetskaya@gmail.com>
2021-11-13 19:50:26 +0100

    Hide background

--- src/App.vue
+++ src/App.vue
@@ -28,8 +28,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background: url('./assets/images/coconuts.jpg') no-repeat center center fixed !important;
-  background-size: cover;
+  //background: url('./assets/images/coconuts.jpg') no-repeat center center fixed !important;
+  //background-size: contain;
 }
 
 #nav {
